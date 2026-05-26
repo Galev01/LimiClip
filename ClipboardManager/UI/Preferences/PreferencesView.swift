05ef7ab84014d128897131094710afba506b4514
Gal Lev <gal.lev@xmcyber.com>
2026-05-26 12:41:26 +0300

    prefs: add Privacy pane to PreferencesPane enum + PreferencesView

--- ClipboardManager/UI/Preferences/PreferencesView.swift
+++ ClipboardManager/UI/Preferences/PreferencesView.swift
@@ -4,24 +4,33 @@ import SwiftUI
 enum PreferencesPane: String, CaseIterable, Identifiable {
     case general
     case shortcuts
+    case privacy
 
     var id: String { rawValue }
     var label: String {
         switch self {
-        case .general: return "General"
+        case .general:   return "General"
         case .shortcuts: return "Shortcuts"
+        case .privacy:   return "Privacy"
         }
     }
     var symbol: String {
         switch self {
-        case .general: return "gearshape"
+        case .general:   return "gearshape"
         case .shortcuts: return "keyboard"
+        case .privacy:   return "hand.raised"
         }
     }
 }
 
 struct PreferencesView: View {
+    let exclusionsVM: ExclusionsViewModel
+
     @State private var selected: PreferencesPane = .general
+
+    init(exclusionsVM: ExclusionsViewModel) {
+        self.exclusionsVM = exclusionsVM
+    }
 
     var body: some View {
         NavigationSplitView {
@@ -36,6 +45,7 @@ struct PreferencesView: View {
                 switch selected {
                 case .general:   GeneralPane()
                 case .shortcuts: ShortcutsPane()
+                case .privacy:   PrivacyPane(viewModel: exclusionsVM)
                 }
             }
             .frame(minWidth: 380, maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
@@ -45,6 +55,8 @@ struct PreferencesView: View {
 }
 
 #Preview {
-    PreferencesView()
+    let store = try! ClipboardStore(configuration: ClipboardStore.testingConfiguration())
+    let vm = ExclusionsViewModel(store: store)
+    return PreferencesView(exclusionsVM: vm)
         .frame(width: 600, height: 400)
 }
